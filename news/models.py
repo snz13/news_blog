3378a4545b4b43bbb785e2ab0c001d82caf1acea
sanzes13 <sanzes13@gmail.com>
2022-02-17 15:34:39 +0300

    Create an app news, build models, first templates

---
+++ news/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class News(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название')
+    content = models.TextField(blank=True, verbose_name='Содержание')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Изменено')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Изображение', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']
---
+++ news/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import News
+
+
+def index(request):
+    news = News.objects.all()
+    return render(request, 'news/index.html', {'news': news, 'title': 'List of articles'})
