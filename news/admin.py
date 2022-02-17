8a25d75eedea2da4f7b0fc4b36329d1eaf152348
sanzes13 <sanzes13@gmail.com>
2022-02-18 01:40:40 +0300

    add new model, migrations, add field to news model

--- news/admin.py
+++ news/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 
 from .models import News
+from .models import Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published', 'category')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', )
+    list_display_links = ('id', 'title', )
+    search_fields = ('title', )
+
+
 admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
