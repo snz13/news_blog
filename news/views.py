c150a546cf27484caeb5e5fdc21a633fa4bec28d
sanzes13 <sanzes13@gmail.com>
2022-02-23 13:22:13 +0300

    Template changes: add bootstrap, news cards

--- news/views.py
+++ news/views.py
@@ -6,4 +6,4 @@ from .models import News
 
 def index(request):
     news = News.objects.all()
-    return render(request, 'news/index.html', {'news': news, 'title': 'List of articles'})
+    return render(request, 'news/index.html', {'news': news, 'title': 'News blog'})
