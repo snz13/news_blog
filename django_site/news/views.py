85f0b4eff1f101d2b04f2bac8da3b23c3c80b4af
sanzes13 <sanzes13@gmail.com>
2022-03-01 02:09:31 +0300

    templates changes

--- django_site/news/views.py
+++ django_site/news/views.py
@@ -6,20 +6,16 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
     context = {'news': news,
                'title': 'News blog',
-               'categories': categories,
                }
     return render(request, 'news/index.html', context=context)
 
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     context = {'news': news,
-               'categories': categories,
                'category': category,
                }
     return render(request, 'news/category.html', context=context)
