1cc714d5a186c6e3f6a4d9db64d423a0ab9724d7
sanzes13 <sanzes13@gmail.com>
2022-03-03 00:28:26 +0300

    collecting static

--- django_site/django_site/settings.py
+++ django_site/django_site/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'django_site/static')
+]
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
