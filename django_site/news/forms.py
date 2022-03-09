11267af3224e861e4deed1c0985d1b4fea3d7a85
sanzes13 <sanzes13@gmail.com>
2022-03-09 16:02:09 +0300

    add non-orm form

--- django_site/news/forms.py
+++ django_site/news/forms.py
@@ -3,7 +3,9 @@ from .models import Category
 
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    content = forms.CharField()
-    if_published = forms.BooleanField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=150, label='Название', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    content = forms.CharField(label='Содержание', required=False,
+                              widget=forms.Textarea(attrs={'class': 'form-control'}))
+    is_published = forms.BooleanField(label='Опубликовано', initial=True)
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label=None,
+                                      widget=forms.Select(attrs={'class': 'form-control'}))
