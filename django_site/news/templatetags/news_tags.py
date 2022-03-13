9da78dacec46f794cea10774971832782cba0b01
sanzes13 <sanzes13@gmail.com>
2022-03-13 14:00:48 +0300

    update category show

--- django_site/news/templatetags/news_tags.py
+++ django_site/news/templatetags/news_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 from news.models import Category
 
@@ -7,4 +8,4 @@ register = template.Library()
 
 @register.simple_tag()
 def get_categories():
-    return Category.objects.all()
+    return Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
--- django_site/news/views.py
+++ django_site/news/views.py
@@ -41,35 +41,3 @@ class ViewNews(DetailView):
 class CreateNews(CreateView):
     form_class = NewsForm
     template_name = 'news/add_news.html'
-# def index(request):
-#     news = News.objects.all()
-#     context = {'news': news,
-#                'title': 'News blog',
-#                }
-#     return render(request, 'news/index.html', context=context)
-#
-#
-# def get_category(request, category_id):
-#     news = News.objects.filter(category_id=category_id)
-#     category = Category.objects.get(pk=category_id)
-#     context = {'news': news,
-#                'category': category,
-#                }
-#     return render(request, 'news/category.html', context=context)
-#
-#
-# def view_news(request, news_id):
-#     news_item = get_object_or_404(News, pk=news_id)
-#     context = {'news_item': news_item}
-#     return render(request, 'news/view_news.html', context=context)
-#
-#
-# def add_news(request):
-#     if request.method == 'POST':
-#         form = NewsForm(request.POST)
-#         if form.is_valid():
-#             news = form.save()
-#             return redirect(news)
-#     else:
-#         form = NewsForm()
-#     return render(request, 'news/add_news.html', context={'form': form})
