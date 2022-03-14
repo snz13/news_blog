51db204b36a0ba8c9d575e55c3049c9fe3d91871
sanzes13 <sanzes13@gmail.com>
2022-03-15 01:01:39 +0300

    admin panel customization

--- django_site/news/admin.py
+++ django_site/news/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import News
 from .models import Category
@@ -8,13 +9,24 @@ class NewsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published', 'category')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
+    fields = ('title', 'created_at', 'updated_at', 'content', 'photo', 'get_photo', 'is_published', 'category')
+    readonly_fields = ('created_at', 'updated_at', 'get_photo')
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="75">')
+        else:
+            return '-'
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', )
-    list_display_links = ('id', 'title', )
-    search_fields = ('title', )
+    list_display = ('id', 'title',)
+    list_display_links = ('id', 'title',)
+    search_fields = ('title',)
 
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_title = 'Управление записями новостей'
+admin.site.site_header = 'Управление записями новостей'
--- django_site/news/views.py
+++ django_site/news/views.py
@@ -14,7 +14,7 @@ class HomeNews(ListView):
         return context
 
     def get_queryset(self):
-        return News.objects.filter(is_published=True)
+        return News.objects.filter(is_published=True).select_related('category')
 
 
 class NewsByCategory(ListView):
@@ -29,7 +29,7 @@ class NewsByCategory(ListView):
         return context
 
     def get_queryset(self):
-        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
+        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True).select_related('category')
 
 
 class ViewNews(DetailView):
