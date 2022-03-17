5c94e85efa0b20258dd5033fb832869c9210f9be
sanzes13 <sanzes13@gmail.com>
2022-03-17 12:18:15 +0300

    add registration form

--- django_site/news/urls.py
+++ django_site/news/urls.py
@@ -4,6 +4,8 @@ from .views import *
 
 urlpatterns = [
     # path('', index, name='home'),
+    path('register', register, name='register'),
+    path('login', login, name='login'),
     path('', HomeNews.as_view(), name='home'),
     path('category/<int:category_id>/', NewsByCategory.as_view(), name='category'),
     path('news/<int:pk>/', ViewNews.as_view(), name='view_news'),
--- django_site/news/views.py
+++ django_site/news/views.py
@@ -1,7 +1,28 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
 
 from .models import News
 from .forms import NewsForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегистрировались')
+            return redirect('login')
+        else:
+            messages.error(request, 'Ошибка регистрации')
+    else:
+        form = UserCreationForm()
+    return render(request, 'news/register.html', {'form': form})
+
+
+def login(request):
+    return render(request, 'news/login.html')
 
 
 class HomeNews(ListView):
